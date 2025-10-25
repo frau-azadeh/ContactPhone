e387c9da5b93eeffbeb9a4c8ef6774f6a13ef1dd
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-25 16:00:29 +0330

    update form

--- ContactPhone/frmAddOrEdit.cs
+++ ContactPhone/frmAddOrEdit.cs
@@ -11,15 +11,76 @@ using System.Windows.Forms;
 namespace ContactPhone
 {
     public partial class frmAddOrEdit : Form
+       
     {
+        IContactsRepository repositor;
         public frmAddOrEdit()
         {
             InitializeComponent();
+            repositor = new ContactsRepository();
+
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void frmAddOrEdit_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void frmAddOrEdit_Load(object sender, EventArgs e)
+        {
+            this.Text = "افزودن شخص جدید";
+        }
+
+         bool ValidateInputs()
+        {
+            bool isValid = true;
+            if(textName.Text == "")
+            {
+               
+                MessageBox.Show("لطفا نام را وارد کنید", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if(textFamily.Text == "")
+            {
+                MessageBox.Show("لطفا نام خانوادگی را وارد کنید", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if(textAddress.Text == "")
+            {
+                MessageBox.Show("لطفا آدرس را وارد کنید", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if(textMobile.Text == "")
+            {
+                MessageBox.Show("لطفا شماره موبایل را وارد کنید", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if(textPhon.Text == "")
+            {
+                MessageBox.Show("لطفا شماره تلفن را وارد کنید", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+        
+            return isValid;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                bool isSuccess = repositor.Insert(textName.Text, textFamily.Text, textAddress.Text, textMobile.Text, textPhon.Text);
+                if (isSuccess == true)
+                {
+                    MessageBox.Show("عملیات با موفقیت قیت شد", "موفقیت", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    DialogResult = DialogResult.OK;
+                }
+                else
+                {
+                    MessageBox.Show("عملیات با شکست مواجه شد", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
