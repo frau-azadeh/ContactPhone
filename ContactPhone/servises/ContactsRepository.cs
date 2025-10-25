bc860d4b1c8061033c70384cb20c52f09fdbd7e4
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-25 11:29:59 +0330

    update name

---
+++ ContactPhone/servises/ContactsRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace ContactPhone 
+{
+    class ContactsRepository : IContactsRepository
+    {
+        public bool Delete(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(string firstName, string lastName, string address, string mobile, string telephon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SelectAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SelectRow(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int contactId, string firstName, string lastName, string address, string mobile, string telephon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
