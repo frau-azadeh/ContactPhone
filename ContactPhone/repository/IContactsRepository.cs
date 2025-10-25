d012ffcd2317dfff5f09282b65ebff37d5719ffa
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-25 11:12:50 +0330

    add interface[selectAll, selectRow, update, delete, insert]

---
+++ ContactPhone/repository/IContactsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ContactPhone.repository
+{
+    interface IContactsRepository
+    {
+        bool Insert(string firstName, string lastName, string address, string mobile, string telephon);
+        bool Update(int contactId, string firstName, string lastName, string address, string mobile, string telephon);
+        bool Delete(int contactId);
+        DataTable SelectAll();
+        DataTable SelectRow(int contactId);
+
+       
+    }
+}
