7fdcb3c99b391c24bd818404f20b5651ec04b45b
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-26 11:23:17 +0330

    add edit

--- ContactPhone/MainForm.Designer.cs
+++ ContactPhone/MainForm.Designer.cs
@@ -175,6 +175,7 @@
             this.btnEdit.TabIndex = 6;
             this.btnEdit.Text = "ویرایش";
             this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
             // MainForm
             // 
